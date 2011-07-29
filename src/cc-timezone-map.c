3068ad5c94c24590d2c37b41ea5c4d0fcfcbdb1f
Evan Dandrea <evan.dandrea@canonical.com>
2011-07-29 10:01:38 +0100

    Put data in the right place.

--- src/cc-timezone-map.c
+++ src/cc-timezone-map.c
@@ -983,7 +983,7 @@ load_backward_tz (CcTimezoneMap *self)
 
   self->priv->alias_db = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, g_free);
 
-  if (g_file_get_contents (GNOMECC_DATA_DIR "/datetime/backward", &contents, NULL, &error) == FALSE)
+  if (g_file_get_contents (GNOMECC_DATA_DIR "/backward", &contents, NULL, &error) == FALSE)
     {
       g_warning ("Failed to load 'backward' file: %s", error->message);
       return;
